83745ca7d861f97b800c9a0f00fc88f467421433
Maxhy <maxime.chamley@leosac.com>
2024-03-12 17:53:32 +0100

    Add ServerInfo API

---
+++ CredentialProvisioning.API/IServerInfoAPI.cs
@@ -0,0 +1,14 @@
+﻿using Refit;
+
+namespace Leosac.CredentialProvisioning.API
+{
+    public interface IServerInfoAPI
+    {
+        [Headers("Authorization: Bearer")]
+        [Get("/GetAttributes")]
+        Task<string> GetAttributes();
+
+        [Get("/GetAPIVersion")]
+        Task<string> GetAPIVersion();
+    }
+}
